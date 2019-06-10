e9af9a633e288c8cba718dcffdad398e6f12ad84
Leonard <leoek2011@gmail.com>
2019-06-10 19:06:54 +0200

    fix hypertunnel server arg

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ARGS="-p ${PORT} -s ${DOMAIN} -t ${TOKEN} $@"
+ARGS="-p ${PORT} -d ${DOMAIN} -t ${TOKEN} $@"
 echo "Starting hypertunnel server with hypertunnel-server $ARGS"
 hypertunnel-server $ARGS
