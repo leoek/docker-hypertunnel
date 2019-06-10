3cee25a3eec63a5b80ae047da0cf1cf8ce53371d
Leonard <leoek2011@gmail.com>
2019-06-10 18:54:02 +0200

    add internet port env

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-ARGS="-p ${PORT} -s ${SERVER} -t ${TOKEN} -l ${HOST} $@"
+ARGS="-p ${PORT} -s ${SERVER} -t ${TOKEN} -l ${HOST}"
+if [ ! -z "$INTERNET_PORT" ]
+then
+      ARGS="$ARGS -i $INTERNET_PORT"
+fi
+ARGS="$ARGS $@"
 echo "Starting hypertunnel with hypertunnel $ARGS"
 hypertunnel $ARGS
